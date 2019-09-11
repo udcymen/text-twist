1ea8e29946bd8205018dd25e68c318345e162d71
Owen Li <Owen@wifi-roaming-128-4-125-201.host.udel.edu>
2019-09-10 20:59:45 -0400

    almost done

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 $(function () {
+
+    var source = "";
+
     let showRacks = function (racks) {
         $("#bingos").html('');
         racks.map(rack => {
@@ -8,12 +11,56 @@ $(function () {
             $(evt.currentTarget).find(".answer").toggleClass("d-none");
         });
     }
+    
+    var count = 120;
 
+    var counter = setInterval(timer, 1000);
+    function timer() {
+        count = count - 1;
+        $("#timer").html("Time left: " + count + " secs");
+        if (count <= 0) {
+            clearInterval(counter);
+            alert("you are out.");
+            $(".title").removeClass("d-none");
+            $("#content").addClass("d-none");
+            return;
+        }
+    }
+
+    $("#startBtn").on("click", function () {
+        $(".title").addClass("d-none");
+        $("#content").removeClass("d-none");
+
+
+        function makeid() { 
+            var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+            return characters.charAt(Math.floor(Math.random() * characters.length));
+        }
+
+
+        for (var i = 0; i < 5; i++) {
+            var letter = makeid();
+            source += letter;
+            $("#select").append("<span class='mx-3 card'>letter</span>");
+        }
+    });
+
+    $("#guess").on('keypress', function (evt) {
+        if (evt.which == 13) {
+            evt.preventDefault();
+            alert(evt.currentTarget.value);
+            $("#guess").val("");
+            return false;
+        }
+    });
+    
     $("#grabmore").on("click", function () {
         $.ajax({
             method: "GET",
             url: "http://localhost:8080/text-twist/api.php",
-            success: data=>{ showRacks(data)}
+            success: data => { showRacks(data) }
         });
     });
+
 });
