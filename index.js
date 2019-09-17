4bbce0c8242572197938b074419e5b813e7a308d
Owen Li <Owen@wifi-roaming-128-4-124-152.host.udel.edu>
2019-09-17 19:07:22 -0400

    9.17 update

--- index.js
+++ index.js
@@ -11,50 +11,65 @@ $(function () {
             $(evt.currentTarget).find(".answer").toggleClass("d-none");
         });
     }
-    
-    var count = 120;
-
-    var counter = setInterval(timer, 1000);
-    function timer() {
-        count = count - 1;
-        $("#timer").html("Time left: " + count + " secs");
-        if (count <= 0) {
-            clearInterval(counter);
-            alert("you are out.");
-            $(".title").removeClass("d-none");
-            $("#content").addClass("d-none");
-            return;
-        }
-    }
 
     $("#startBtn").on("click", function () {
-        $(".title").addClass("d-none");
+
+        var count = 120;
+
+        var counter = setInterval(timer, 1000);
+        function timer() {
+            count = count - 1;
+            $("#timer").html("Time left: " + count + " secs");
+            if (count <= 0) {
+                clearInterval(counter);
+                alert("you are out. Please start again.");
+                location.reload();
+                // $(".title").removeClass("d-none");
+                //$("#content").addClass("d-none");
+                return;
+            }
+        }
         $("#content").removeClass("d-none");
 
+        $("#startBtn").attr("disabled", true);
 
-        function makeid() { 
+        function makeid() {
             var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
             return characters.charAt(Math.floor(Math.random() * characters.length));
         }
 
-
-        for (var i = 0; i < 5; i++) {
+        for (var i = 0; i < 6; i++) {
             var letter = makeid();
             source += letter;
-            $("#select").append("<span class='mx-3 card'>letter</span>");
+            $("#select").append(letter);
         }
     });
 
+    $('#reset').click(function() {
+        location.reload();
+    });
+
     $("#guess").on('keypress', function (evt) {
+        
+        function isNumeric(t) {
+            var regex = /\d/g;
+            return regex.test(t);
+        }
         if (evt.which == 13) {
             evt.preventDefault();
-            alert(evt.currentTarget.value);
+            if (isNumeric(evt.currentTarget.value)) {
+                alert('Please enter only letters.');
+            }
+            else
+                $("#guessWord").append(evt.currentTarget.value.toUpperCase()).append("<br>");
+
+            //alert(evt.currentTarget.value);
             $("#guess").val("");
             return false;
         }
     });
-    
+
     $("#grabmore").on("click", function () {
         $.ajax({
             method: "GET",
