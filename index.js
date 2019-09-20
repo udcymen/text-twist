03030006669c11d2322f49956cef61fd51f98272
Owen Li <Owen@Owens-MacBook-Pro.local>
2019-09-19 20:53:12 -0400

    finished

--- index.js
+++ index.js
@@ -8,20 +8,24 @@ $(function () {
         for (let i = 0; i < data[0].rack.length; i++) {
             $("#select").append("<text class='mx-3'>" + data[0].rack[i] + "</text>");
         }
-        
+
         answers = data[0].words;
 
         var count = 120;
-        var counter = setInterval(function (){
+        var counter = setInterval(function () {
             count = count - 1;
             $("#timer").html("Time left: " + count + " secs");
             if (count <= 0) {
                 clearInterval(counter);
-                alert("Game Over");
-                location.reload();
+                $('#scoreMessage').text('Your score is ' + $("#Point-Value").text());
+                $('#modalInfotime').modal('toggle');
+                $('.reset').click(function () {
+                    location.reload();
+                });
                 return;
             }
         }, 1000);
+        $("#Point").append("Scores: <text id='Point-Value'>0</text>");
     }
 
     $("#startBtn").on("click", function () {
@@ -34,43 +38,43 @@ $(function () {
         });
     });
 
-    $('#reset').click(function() {
+    $('#reset').click(function () {
         location.reload();
     });
 
 
     $("#guess").on('keypress', function (evt) {
         if (evt.which == 13) {
-            
+
             let regex = /^[a-zA-Z]*$/;
             let guess = $("#guess").val().toUpperCase();
-            if(answers.length == 0){
+            if (answers.length == 0) {
                 $('#modalInfosucess').modal('toggle');
-                $('.reset').click(function() {
+                $('.reset').click(function () {
                     location.reload();
                 });
-                //location.reload();
             }
 
-            if (regex.test(guess)) { 
-                if (answers.includes(sha256(guess))){
+            if (regex.test(guess)) {
+                if (answers.includes(sha256(guess))) {
                     answers.splice(answers.indexOf(sha256(guess)), 1);
                     $("#guessWord").append($("#guess").val().toUpperCase()).append("<br>");
+                    $("#Point-Value").text(parseInt($("#Point-Value").text()) + 1)
                     $('#guess').attr("placeholder", answers.length + " words left");
                 }
             }
-            else{
+            else {
                 alert('Please enter only letters.');
             }
 
             $("#guess").val("");
 
-            
+
             evt.preventDefault();
             return false;
         }
-        
+
     });
 
-    
+
 });
